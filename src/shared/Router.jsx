5227f93ff6c47af4406de7c1882ad7176ea1e11d
jaeyeoneej <5ikve@naver.com>
2022-10-30 01:35:59 +0900

    키워드:   #00
    변경사항 :
    
    # 키워드: 생성, 수정, 추가,고치기,문서화,스타일, 테스트
    # "왜", "무엇"을 포함하기
    # 제목은 80자 이내로, 긴 내용은 줄바꿈하고 본문에서

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import SignupJJY from "../components/SignupJJY";
 import HomePage from "../pages/HomePage"
 
 const Router = () => {
@@ -8,9 +7,8 @@ const Router = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                {/* <Route path="/login" element={<Login />} /> */}
-                {/* <Route path="/signup" element={<SignupJJY />} /> */}
             </Routes>
         </BrowserRouter>
     )
 }
+export default Router
